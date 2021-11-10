2e0830f5a04c6796ef69b4e985b3a9ba577c8f07
Wenwen Yu <yuwenwen62@gmail.com>
2021-11-10 20:19:18 +0800

    Update dist_train.sh

--- dist_train.sh
+++ dist_train.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 python -m torch.distributed.launch --nnodes=1 --node_rank=0 --nproc_per_node=4 \
 --master_addr=127.0.0.1 --master_port=5555 \
-train.py -c config.json -d 1,2,3,4 --local_world_size 4
+train.py -c configs/config.json -d 0,1,2,3 --local_world_size 4
